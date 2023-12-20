2d401c5f954fa3c09c32169b451ca0c9fb6e9817
Allan Musyoka <musyokaallan96@gmail.com>
2023-12-20 18:16:21 +0300

    Added call to action

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ function Button() {
 
     return(
 
-        <button className="px-[30px] py-3 rounded-[100px] text-white bg-pink font-openSans font-bold text-[.8rem] mb-10">Get Started For Free</button>
+        <button className="px-[30px] py-3 rounded-[100px] text-white bg-pink font-openSans font-bold text-[.8rem]">Get Started For Free</button>
 
     )
 }
--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,8 +1,20 @@
 /* eslint-disable react-refresh/only-export-components */
 
+import Button from "./Button";
+
 function CTA() {
     return(
-        <h1>CTA</h1>
+        <section>
+
+            <div className="text-center border-4 border-red-500 max-w-[90%] ss:max-w-[50%] ss:w-[600px] mx-auto py-[40px]">
+
+                <h1 className="capitalize mb-5 font-poppins font-bold">Ready to build your community ?</h1>
+
+                <Button />
+
+            </div>
+
+        </section>
     )
 }
 
